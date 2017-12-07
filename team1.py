41b8a5de9b5e68db9704bcbfe4e5af98b0216461
makayla cizek <229134@shakopeeschools.org>
2017-12-07 08:34:25 -0600

    Worked on basic stats

--- team1.py
+++ team1.py
@@ -1,30 +1,41 @@
-#Makayla test cat roseGold a
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+#Makayla
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'no bueno'
+strategy_name = 'yah'
+strategy_description = 'How does this strategy decide?'  
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''basic start'''
+    if len(my_history) == 0:
+        return 'b'
+    if len(my_history) == 1:
+        return 'c'
+        
+    '''checks the basic c,b,c,b,c,b pattern'''
+    if their_history[-1] == their_history[-3]:
+        if their_history[-1] == 'c':
+            return 'b'
+        elif their_history[-1] == 'b':
+            return 'c'
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    '''checks the c,b,b pattern'''
+    if their_history[-1] == their_history[-4]:
+        if their_history[-1] == 'c':
+            return 'b'
+        elif their_history[-1] == 'b':
+            return 'c'
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    '''checks cccb pattern'''
+    if their_history[-1] == their_history[-5]:
+        if their_history[-1] == 'b':
+            return 'c'
+        if their_history[-1] == 'c':
+            return 'b'
+        
+    '''basic checks'''    
+    if their_history[-1] and their_history[-2] == 'c':
+        return 'b'
+    if their_history[-1] and their_history[-2] == 'b':
+        return 'c'
+    if their_history[-3] == 'b':
+        return 'b'
