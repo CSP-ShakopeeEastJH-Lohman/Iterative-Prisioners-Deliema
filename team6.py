3c7527b025529932a1a5e31c840b421665a19b5b
Bennett Goltz <224779@c1mtl517h3qd.isd720.com>
2017-12-12 08:22:15 -0600

    Finished code...may need some adjustments.

--- team6.py
+++ team6.py
@@ -12,21 +12,20 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
     if len(my_history)== 0:
         return 'c'
-    if len(their_history)==-1:
+    if their_history[-1]== 'b':
+        return 'b'
+    if len(my_history) < 2 and their_history[-1]== 'c':
+        return'c'
+    if their_history[-2] == 'c' and their_history[-1]=='c':
         return 'c'
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+        my_score += 1100
+    if their_history[-1]=='b':
+        return 'b'
+    if len(my_history) ==199:
+        return 'b'
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+    
+
+    
