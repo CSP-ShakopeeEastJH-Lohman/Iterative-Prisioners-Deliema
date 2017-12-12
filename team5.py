4dc79ceb471e28701e370ef23fb12335c78d7055
JLundquist14 <235440@shakopeeschools.org>
2017-12-12 08:23:56 -0600

    First round
    Rough draft

--- team5.py
+++ team5.py
@@ -1,24 +1,27 @@
 #Chloe & Josh
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
-####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Chloe and Josh' 
+strategy_name = 'Betrayal at last.'
+strategy_description = 'If our opponent colludes every time we do as well except for the last round where we return betray.'
     
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
+def move(my_history, their_history, my_score, their_score): 
+    if len(my_history) < 3:
+        return 'c'
+        if their_history[-1] == 'c' and their_history[-2] == 'c':
+            return 'c'
+    if len(their_history) > 5:
+        return 'c'
+    else:
+        if their_history[-1] == 'b'or their_history[-2] == 'b':
+            return 'b'
+        elif their_score > 0 and their_history[-1] == 'b' or 'c':
+            return 'b'
+        else:
+            return 'c'
+        if their_history[-199] == 'c':
+            return 'b'
+            
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
